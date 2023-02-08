9b80ff69e0054ee34a5a6d47a3d540478944988c
Koji Osugi <koji097@gmail.com>
2023-02-08 11:41:50 -0300

    Adjustment

--- install-hooks.bash
+++ install-hooks.bash
@@ -2,6 +2,6 @@
 GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks…"
 echo "Pre-Push"
-ln -s ../../scripts/pre-push.bash $GIT_DIR/hooks/pre-push
+ln -s pre-push.bash $GIT_DIR/hooks/pre-push
 
 echo “Done!”
